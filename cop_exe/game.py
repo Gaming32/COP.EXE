371495e3ec33f07b2eae6c7970d9b9bb68fc56aa
Gaming32 <soujournme@gmail.com>
2021-04-01 11:55:43 -0500

    enemy movement

--- cop_exe/game.py
+++ cop_exe/game.py
@@ -1,8 +1,32 @@
-from cop_exe.level_data import Coordinate, LEVEL, MAX_COORDS
+import random
+
+from cop_exe.level_data import Coordinate, LEVEL, MAX_COORDS, Node
 from pygame import Surface, Vector2
 
 from cop_exe.consts import CHARACTER_OPACITY, ENEMY_COLOR, PLAYER_COLOR
 from cop_exe.pygame_import import *
+
+
+def get_movement(choices, name, cur, amnt):
+    if choices is None:
+        if name == 'left':
+            return (cur[0] - amnt, cur[1])
+        elif name == 'right':
+            return (cur[0] + amnt, cur[1])
+        elif name == 'up':
+            return (cur[0], cur[1] - amnt)
+        elif name == 'down':
+            return (cur[0], cur[1] + amnt)
+
+
+def get_enemy_move(node: Node, enemy: tuple, player: tuple, dirs: list, dir: str):
+    choices = getattr(node, dir)
+    if choices is None:
+        next = get_movement(choices, dir, enemy, 1)
+    else:
+        next = choices[0]
+    if next is not None and next != player:
+        dirs.append(next)
 
 
 class Game:
@@ -63,7 +87,25 @@ class Game:
                 pygame.draw.circle(psurf, PLAYER_COLOR, player_pos.render, 10)
             psurf.set_alpha(CHARACTER_OPACITY)
             surf.blit(psurf, (0, 0))
-    
+
+    def move_enemy(self):
+        if self.player == self.enemy:
+            yield from global_vars.text_box.slow_print('You win!')
+        curnode = LEVEL.get(self.enemy)
+        if curnode is None:
+            return
+        dirs = []
+        if self.enemy[0] > 0:
+            get_enemy_move(curnode, self.enemy, self.player, dirs, 'left')
+        if self.enemy[0] < MAX_COORDS[0]:
+            get_enemy_move(curnode, self.enemy, self.player, dirs, 'right')
+        if self.enemy[1] > 0:
+            get_enemy_move(curnode, self.enemy, self.player, dirs, 'up')
+        if self.enemy[1] < MAX_COORDS[1]:
+            get_enemy_move(curnode, self.enemy, self.player, dirs, 'down')
+        move = random.choice(dirs)
+        self.enemy = move
+
     def player_move(self, name: str, amnt: int):
         if amnt > 2:
             yield from global_vars.text_box.slow_print("Can't move", name, 'more than 2 blocks')
@@ -74,20 +116,14 @@ class Game:
         node = LEVEL[self.player]
         choices = getattr(node, name)
         if choices is None:
-            if name == 'left':
-                next = (self.player[0] - amnt, self.player[1])
-            elif name == 'right':
-                next = (self.player[0] + amnt, self.player[1])
-            elif name == 'up':
-                next = (self.player[0], self.player[1] - amnt)
-            elif name == 'down':
-                next = (self.player[0], self.player[1] + amnt)
+            next = get_movement(choices, name, self.player, amnt)
         else:
             next = getattr(node, name)[amnt - 1]
         if next is None or next[0] > MAX_COORDS[0] or next[0] < 0 or next[1] > MAX_COORDS[1] or next[1] < 0:
             yield from global_vars.text_box.slow_print('There is no road leading in that direction')
             return
         self.player = next
+        yield from self.move_enemy()
 
     def move_left(self, amnt: int):
         yield from self.player_move('left', amnt)
